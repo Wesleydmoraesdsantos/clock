3f37d26c1ffcad878b59c5ccc64812209ede2c38
Wesley3king <97459154+Wesley3king@users.noreply.github.com>
2022-01-22 17:05:43 -0300

    let's go! baby

--- index.js
+++ index.js
@@ -32,28 +32,32 @@ var control = setInterval(() => {
     let hor = time.getHours();
     let min = time.getMinutes();
     let sec = time.getSeconds();
-    let day = time.geyDay();
+    let day = time.getDay();
     let mon = time.getMonth();
     let year = time.getFullYear();
 
     background(hor);
-    if( hor < 10) {
+    if (hor < 10) {
         hor = `0${hor}`;
     }
-    if( min < 10) {
+    if (min < 10) {
         min = `0${min}`;
     }
-    if( sec < 10) {
+    if (sec < 10) {
         sec = `0${sec}`;
     }
     local.innerHTML = `${hor} : ${min} : ${sec} <br> ${day} / ${mon} / ${year}`;
 }, 1000);
-async function background (v) {
-   if( v >= 6 && v < 12) {
-       lugar_background.style.backgroundimage= "url(./img/day.jpg)";
-   }else if( v >= 12 && v < 18) {
-    lugar_background.style.backgroundimage= "url(./img/afternoon.jpg)";
-   }else if( v >= 18 && v < 6) {
-    lugar_background.style.backgroundimage= "url(./img/night.jpg)";
-   }
+function background(v) {
+    console.log("ok");
+    if (v >= 6 && v < 12) {
+        lugar_background.style.backgroundImage = "url(./img/day.jpg)";
+        console.log("day");
+    } else if (v >= 12 && v < 18) {
+        lugar_background.style.background= "url(./img/afternoon.jpg)";
+        console.log("afternoon");
+    } else if (v >= 18 && v < 6) {
+        lugar_background.style.backgroundImage = "url(./img/night.jpg)";
+        console.log("night");
+    }
 }
